89c92c6471df2b2932c7e2d92535c093c6947a42
Kevin Adu-Sarkodie <kevyugi@gmail.com>
2023-10-06 11:55:40 +0900

    Corrected branch for CMS

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,7 +21,7 @@ export default defineConfig({
       config: {
         backend: {
           name: 'git-gateway',
-          branch: 'main',
+          branch: 'master',
         },
         collections: [
           {
