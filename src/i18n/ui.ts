037b8e71b640b5cd3e4daf1d2a309b450a04890e
Kevin Adu-Sarkodie <kevyugi@gmail.com>
2024-01-11 12:41:09 +0900

    Translations - #18

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -9,11 +9,21 @@ export const defaultLang = 'ja';
 
 export const ui = {
   en: {
-    'tabs.gallery': 'Gallery',
+    'tabs.photos': 'Photos',
     'tabs.albums': 'Albums',
+    'home.tagline': 'It has been this long since our son Leo was brought into the world!',
+    'more_photos': 'More Photos',
+    'more_albums': 'More Albums',
+    'album.before_birth': 'Before Birth',
+    'album.few_days_old': 'A few days old'
   },
   ja: {
-    'tabs.gallery': 'Gallery',
-    'tabs.albums': 'Albums',
+    'tabs.photos': '写真',
+    'tabs.albums': 'アルブム',
+    'home.tagline': 'レオが私達のもとに誕生してから',
+    'more_photos': 'もっと見る',
+    'more_albums': 'もっと見る',
+    'album.before_birth': 'Before Birth',
+    'album.few_days_old': 'A few days old'
   },
 } as const;
