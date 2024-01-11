afc20e52c2ebf744f58604e48e35055fdd424125
Kevin Adu-Sarkodie <kevyugi@gmail.com>
2024-01-11 17:54:15 +0900

    Added translations  - #18

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -9,21 +9,31 @@ export const defaultLang = 'ja';
 
 export const ui = {
   en: {
-    'tabs.photos': 'Photos',
-    'tabs.albums': 'Albums',
+    'common.photos': 'Photos',
+    'common.albums': 'Albums',
+    'tabs.wishlist': 'Leo\'s Amazon Wishlist',
     'home.tagline': 'It has been this long since our son Leo was brought into the world!',
+    'count.photos': 'photos',
     'more_photos': 'More Photos',
     'more_albums': 'More Albums',
     'album.before_birth': 'Before Birth',
-    'album.few_days_old': 'A few days old'
+    'album.few_days_old': 'A few days old',
+    'album.few_weeks_old': 'A few weeks old',
+    'photos.tagline': 'Leo\'s life covered in photography',
+    'albums.tagline': 'Moments of Leo\'s life from different ages',
   },
   ja: {
-    'tabs.photos': '写真',
-    'tabs.albums': 'アルブム',
+    'common.photos': 'フォト',
+    'common.albums': 'アルブム',
+    'tabs.wishlist': 'レオのアマゾンウィッシュリスト',
     'home.tagline': 'レオが私達のもとに誕生してから',
+    'count.photos': '枚',
     'more_photos': 'もっと見る',
     'more_albums': 'もっと見る',
-    'album.before_birth': 'Before Birth',
-    'album.few_days_old': 'A few days old'
+    'album.before_birth': '生まれるまで',
+    'album.few_days_old': 'A few days old',
+    'album.few_weeks_old': 'A few weeks old',
+    'photos.tagline': 'Leo\'s life covered in photography',
+    'albums.tagline': 'Moments of Leo\'s life from different ages',
   },
 } as const;
