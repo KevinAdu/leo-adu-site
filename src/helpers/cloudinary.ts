18ad2be4184475df975bdff6c88561982b54ad9a
Kevin Adu-Sarkodie <kevyugi@gmail.com>
2024-01-12 17:01:18 +0900

    Added cloudinary transformation to show photo page

--- src/helpers/cloudinary.ts
+++ src/helpers/cloudinary.ts
@@ -1,1 +1,4 @@
 export const CLOUDINARY_URL_ROOT = 'https://res.cloudinary.com/dz8vyplpm/image/upload/';
+
+const versionAndFilenameRegex = /\/v.*$/g;
+export const getCloudinaryFilename = (image: string) => image.match(versionAndFilenameRegex);
